135cb9729aa96ca897b50998b3b5b985ebb12eaa
TheLevius <reflevius@gmail.com>
2020-07-27 15:29:49 +0300

    feat: implement hooks state in FirstTask

--- src/components/FirstTask/FirstTask.tsx
+++ src/components/FirstTask/FirstTask.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './FirstTask.module.css';
 import avatar from './../../assets/images/jpg/valakas.jpg';
+import {UsersType, UserType} from '../../types/entities';
 
 export const FirstTask = () => {
+
+    let [users, setUsers] = useState<UsersType>(
+        [
+            {id: 1, name: 'Artem', date: '20:00', msg: 'npm start нажимал?'}
+        ]
+    );
+    const user = users[0];
     return (
         <div className={styles.wrap}>
             <div className={styles.box}>
@@ -11,12 +19,12 @@ export const FirstTask = () => {
                 </div>
                 <div className={styles.colMsg}>
                     <div className={styles.mountMsg}>
-                        <h3 className={styles.userName}>Artem</h3>
+                        <h3 className={styles.userName}>{user.name}</h3>
                         <span className={styles.msg}>
-                            npm start нажимал?
+                            {user.msg}
                         </span>
                         <div className={styles.boxDate}>
-                            <span>20:00</span>
+                            <span>{user.date}</span>
                         </div>
                     </div>
                 </div>
--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,3 +1,8 @@
-export type MessageType = {
-    text: string
+export type UserType = {
+    id: number
+    name: string
+    msg: string
+    date: string
+    key?: number
 }
+export type UsersType = UserType[];
