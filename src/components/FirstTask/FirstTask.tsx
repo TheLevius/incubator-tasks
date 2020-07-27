df9e713de0fa8a722374271c52f50e6352cadb26
TheLevius <reflevius@gmail.com>
2020-07-27 06:28:01 +0300

    feat: ьфmake Ð up layot of task

--- src/components/FirstTask/FirstTask.tsx
+++ src/components/FirstTask/FirstTask.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import styles from './firstTaskStyles.module.css';
+import styles from './FirstTask.module.css';
+import avatar from './../../assets/images/jpg/valakas.jpg';
 
 export const FirstTask = () => {
     return (
-        <div className={styles.container}>
-            <h1>Hello world!</h1>
+        <div className={styles.wrap}>
+            <div className={styles.box}>
+                <div className={styles.colPic}>
+                    <img src={avatar} alt="avatar"/>
+                </div>
+                <div className={styles.colMsg}>
+                    <div className={styles.mountMsg}>
+                        <h3 className={styles.userName}>Artem</h3>
+                        <span className={styles.msg}>
+                            npm start нажимал?
+                        </span>
+                        <div className={styles.boxDate}>
+                            <span>20:00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
