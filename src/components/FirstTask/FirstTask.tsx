399d98a321b2e5aefd9395fd1c5d10214cf85458
TheLevius <reflevius@gmail.com>
2020-07-27 03:20:45 +0300

    start: implement basic settings

---
+++ src/components/FirstTask/FirstTask.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './firstTaskStyles.module.css';
+
+export const FirstTask = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
+
+export default FirstTask;
