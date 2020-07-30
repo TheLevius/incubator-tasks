1557525aa7c1b0e70b8f7c8bd4a048035583f0a7
TheLevius <reflevius@gmail.com>
2020-07-30 17:08:47 +0300

    Updates

---
+++ precache-manifest.e36d61d8adfae1b770dd6331e40c3386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0580f495e7a10ed2bbb7d744ec09352a",
+    "url": "/incubator-tasks/index.html"
+  },
+  {
+    "revision": "9e8dbbf5f2b7cd866e54",
+    "url": "/incubator-tasks/static/css/main.f3b514b5.chunk.css"
+  },
+  {
+    "revision": "084f970aab2edbb08fb4",
+    "url": "/incubator-tasks/static/js/2.3331d076.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/incubator-tasks/static/js/2.3331d076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8dbbf5f2b7cd866e54",
+    "url": "/incubator-tasks/static/js/main.10f803aa.chunk.js"
+  },
+  {
+    "revision": "40602bce447cb36b0f26",
+    "url": "/incubator-tasks/static/js/runtime-main.6b9928e8.js"
+  },
+  {
+    "revision": "a8967f9de60a2fd7c0d5929d90a33902",
+    "url": "/incubator-tasks/static/media/valakas.a8967f9d.jpg"
+  }
+]);
